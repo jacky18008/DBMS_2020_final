b07ee5f5b19c3296af7121692ae9f9e096e41d44
RainBoltz <philip27020012@gmail.com>
2020-06-29 11:49:46 +0800

    Update util.py
    search result rerank

--- sql_api_server/util.py
+++ sql_api_server/util.py
@@ -17,14 +17,14 @@ def start_sql_connection():
     return conn
 
 
-def search(conn, query, total_each=8, total=12):
+def search(conn, query, total_each=8, total=16):
     result_id = set()
 
     title_results = []
     for cursor in list(conn.execute(f'''
                                     SELECT * 
                                     FROM movie
-                                    WHERE movie.title like "%{query}%" OR movie.storyline like "%{query}%" limit {total_each}
+                                    WHERE movie.title like "%{query}%" limit {total_each}
                                 ''')):
         result_id.add(cursor[0])
         try:
@@ -57,6 +57,44 @@ def search(conn, query, total_each=8, total=12):
         title_results.append(this_result)
     for _ in range(len(title_results) - total_each):
         title_results.append(None)
+
+    desc_results = []
+    for cursor in list(conn.execute(f'''
+                                    SELECT * 
+                                    FROM movie
+                                    WHERE movie.storyline like "%{query}%" limit {total_each}
+                                ''')):
+        result_id.add(cursor[0])
+        try:
+            genres = '/'.join([ c[0] for c in conn.execute("select type from type where movie_id=?",(cursor[0],)) ])
+        except:
+            genres = '(N/A)'
+        try:
+            director = list(conn.execute("select director from director where movie_id=?",(cursor[0],)))[0][0]
+        except:
+            director = '(N/A)'
+        try:
+            company = list(conn.execute("select company from company where movie_id=?",(cursor[0],)))[0][0]
+        except:
+            company = '(N/A)'
+        try:
+            actors = ', '.join([ c[0] for c in conn.execute("select actor_actress from actor where movie_id=?",(cursor[0],)) ])
+        except:
+            actors = '(N/A)'
+        this_result = {
+            'id':       cursor[0],
+            'name':     cursor[2],
+            'genres':   genres,
+            'director': director,
+            'company':  company,
+            'year':     str(cursor[1]),
+            'actors':   actors,
+            'description': "<i>“%s”</i>&nbsp;&nbsp;%s"%(cursor[5], cursor[4]),
+            'img': cursor[6]
+        }
+        desc_results.append(this_result)
+    for _ in range(len(desc_results) - total_each):
+        desc_results.append(None)
 
     type_results = []
     for _cursor in list(conn.execute(f'''
@@ -145,15 +183,19 @@ def search(conn, query, total_each=8, total=12):
     #rerank
     output_results = \
         title_results[:2] + \
+        desc_results[:2] + \
         type_results[:2] + \
         company_results[:2] + \
         title_results[2:4] + \
+        desc_results[2:4] + \
         type_results[2:4] + \
         company_results[2:4] + \
         title_results[4:6] + \
+        desc_results[4:6] + \
         type_results[4:6] + \
         company_results[4:6] + \
         title_results[6:] + \
+        desc_results[6:] + \
         type_results[6:] + \
         company_results[6:]
     try:
@@ -203,7 +245,7 @@ def recommend(conn, user_hash, total=12):
         }
         recs.append(this_result)
     return recs
-    
+
 
 def cold_recommend(conn):
     mids = ['999', '998', '997', '996', '994', '993', '992', '991', '990', '99']
